8fc7e0eeb13abc782798e717476f1961ebdae227
James Lennon <jameslennon321@gmail.com>
2015-01-28 10:21:37 -0800

    Began Users model

---
+++ application/models/Users_model.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_model extends CI_Model{
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function get_user($id){
+		$query = $this->db->query('
+			SELECT
+				*
+			FROM
+				users
+			WHERE
+				users.user_id = ?
+			', array($id));
+		return $query->row();
+	}
+
+	public function add_user($email, $fname, $lname, $phone){
+		$data = array(
+			"email"=>$email,
+			"first_name"=>$fname,
+			"last_name"=>$lname,
+			"phone"=>$phone,
+			);
+		$this->db->insert('users', $data);
+	}
+
+	public function set_img($user_id, $img_url){
+		$query = $this->db->query('SELECT * from users WHERE users.user_id = ?', array($user_id));
+		if($query->num_rows()==0){
+			return FALSE;
+		}
+		$this->db->query('UPDATE users SET users.img_url=? WHERE users.user_id = ?', array($img_url, $user_id));
+	}
+
+}
