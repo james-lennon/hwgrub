480995cc7293dfec6a2c672ee3d733510fb6b480
James Lennon <jameslennon321@gmail.com>
2015-01-29 21:20:25 -0800

    Changed Ratings Model

--- application/models/Ratings_Model.php
+++ application/models/Ratings_Model.php
@@ -1,152 +1,80 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Users_model extends CI_Model{
+class Ratings_model extends CI_Model{
 
 // driver = 0, customer = 1 
 // bad rating = 0, good rating = 1
 
-public function __construct(){
+	public function __construct(){
 		parent::__construct();
 		$this->load->database();
 	}
 
-public function get_Driver_Ratings($user_id)
-{	
-	$query = $this->db->query('
+	public function get_ratings($user_id, $type){
+		$query = $this->db->query('
 			SELECT
 				*
 			FROM
 				ratings
 			WHERE
-				ratings.user_id = ?
-			AND 
+				ratings.user_id = ?         AND 
 				ratings.type = ?
-			', array($user_id, 0);
+			', array($user_id, $type));
 
-	if($query->num_rows()==0){
-			return FALSE;
-	
-	$ratings = $query->result_array(); 
-	return $ratings; 
-}
-
-public function get_Customer_Ratings($user_id)
-{	
-	$query = $this->db->query('
-			SELECT
-				*
-			FROM
-				ratings
-			WHERE
-				ratings.user_id = ?
-			AND 
-				ratings.type = ?
-			', array($user_id, 1);
-
-	if($query->num_rows()==0){
-			return FALSE;
+		return $query->result();
 	}
-	
-	$ratings = $query->result_array(); 
-	return $ratings; 
-}
 
-public function add_Rating($user_id, $rating_text, $type, $value)
-{
-	$query = $this->db->query('SELECT * from users WHERE users.user_id = ?', array($user_id));
-	if($query->num_rows()==0){
-		return FALSE;
+	public function get_driver_ratings($user_id)
+	{	
+		return $this->get_ratings($user_id, 0);
 	}
+
+	public function get_customer_ratings($user_id)
+	{	
+		return $this->get_ratings($user_id, 1);
+	}
+
+	public function add_rating($user_id, $rating_text, $type, $value)
+	{
+		$query = $this->db->query('SELECT * from users WHERE users.user_id = ?', array($user_id));
+		if($query->num_rows()==0){
+			return FALSE;
+		}
 	
-	$data = array(
+		$data = array(
 			"user_id"=>$user_id,
 			"rating_text"=>$rating_text,
 			"type"=>$type,
 			"value"=>$value,
 			);
 		$this->db->insert('ratings', $data);
-}
+	}
 
-public function get_Driver_Record($user_id)
-{
-	$query1 = $this->db->query('
-		SELECT 
-			* 
-		FROM 
-			ratings
-		WHERE 
-			ratings.user_id = ? 
-		AND 
-			ratings.type = ?
-		AND 
-			ratings.value = ?
-			', 
-			array($user_id, 0, 0));
+	public function get_record($user_id, $type){
+		$query = $this->db->query('
+			SELECT
+				(
+					SELECT COUNT(ratings.rating_id) FROM ratings WHERE ratings.user_id = ? AND ratings.type = ? AND ratings.value = 1
+	    		) as good_ratings,
+	    		(
+					select COUNT(ratings.rating_id) FROM ratings WHERE ratings.user_id = ? AND ratings.type = ? AND ratings.value = 0
+	    		) as bad_ratings
+			LIMIT
+				1
+			', array($user_id, $type, $user_id, $type));
 
-		$negative = $query1->num_rows(); 
+		return $query->row();
+	}
 
-		$query2 = $this->db->query('
-		SELECT 
-			* 
-		FROM 
-			ratings
-		WHERE 
-			ratings.user_id = ? 
-		AND 
-			ratings.type = ?
-		AND 
-			ratings.value = ?
-			', 
-			array($user_id, 0, 1));
-		$positive = $query2->num_rows(); 
+	public function get_driver_record($user_id)
+	{
+		return $this->get_record($user_id, 0);
+	}
 
-		$data = array(
-			"positive"=>$positive, 
-			"negative"=>$negative,
-			);
-
-		return $data; 
-}
-
-public function get_Customer_Record($user_id)
-{
-	$query1 = $this->db->query('
-		SELECT 
-			* 
-		FROM 
-			ratings
-		WHERE 
-			ratings.user_id = ? 
-		AND 
-			ratings.type = ?
-		AND 
-			ratings.value = ?
-			', 
-			array($user_id, 1, 0));
-
-		$negative = $query1->num_rows(); 
-
-		$query2 = $this->db->query('
-		SELECT 
-			* 
-		FROM 
-			ratings
-		WHERE 
-			ratings.user_id = ? 
-		AND 
-			ratings.type = ?
-		AND 
-			ratings.value = ?
-			', 
-			array($user_id, 1, 1));
-		$positive = $query2->num_rows(); 
-
-		$data = array(
-			"positive"=>$positive, 
-			"negative"=>$negative,
-			);
-
-		return $data; 
+	public function get_customer_record($user_id)
+	{
+		return $this->get_record($user_id, 1);
+	}
 }
 
