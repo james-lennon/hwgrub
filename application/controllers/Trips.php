4b4e755835b58963e5cf985cf43b8ae093643846
James Lennon <jameslennon321@gmail.com>
2015-02-02 10:27:02 -0800

    Checked login for Trips controller

--- application/controllers/Trips.php
+++ application/controllers/Trips.php
@@ -9,26 +9,31 @@ class Trips extends CI_Controller {
 	}
 
 	public function get_all_trips() {
+		check_auth();
 		$trips = $this->trips_model->get_all_trips();
 		echo json_encode($trips);
 	}
 
 	public function get_all_active_trips() {
+		check_auth();
 		$trips = $this->trips_model->get_all_active_trips();
 		echo json_encode($trips);
 	}
 
 	public function get_user_trips($user_id) {
+		check_auth();
 		$trips = $this->trips_model->get_user_trips($driver_id);
 		echo json_encode($trips);
 	}
 
 	public function get_user_active_trips($user_id) {
+		check_auth();
 		$trips = $this->trips_model->get_user_active_trips($driver_id);
 		echo json_encode($trips);
 	}
 
 	public function add_trip($driver_id, $expiration, $eta, $restaurant) {
+		check_auth();
 		$this->trips_model->add_trip($driver_id, $expiration, $eta, $restaurant);
 	}
 
